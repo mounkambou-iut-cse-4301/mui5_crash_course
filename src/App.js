2d5313ecaa82420856b5a28a67343efe2976d68e
abdelkarim <abdelkarim@iut-dhaka.edu>
2023-10-01 16:43:01 +0100

    Custom Themes with Providers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import './App.css';
 import TourCard from './components/TourCard';
-
+import SearchAppBar from './components/AppBar';
+import cities from './data.json'
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={5}>
+              <SearchAppBar/>
+      <Container sx={{marginY:5}}>
+        {cities.map((city)=>(
+          <>
+          <Typography
+          variant='h4'
+          component="h2"
+          marginTop={5}
+          marginBottom={3}
+          >
+Top {city.name} tours
+          </Typography>
+          <Grid container spacing={5}>
+            {city.tours.map((tour,index)=>(
+              <TourCard tour={tour} key={index}/>
+            ))}
+          </Grid>
+          </>
+        ))}
+        {/* <Grid container spacing={5}>
         <TourCard/>
         <TourCard/>
         <TourCard/>
         <TourCard/>
-        </Grid>
+        </Grid> */}
 
 </Container>
     </div>
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,29 +1,56 @@
 import { Box, Grid, Rating, Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { AccessTime } from '@mui/icons-material';
-
-const TourCard = () => {
-    return <Grid item xs={3}><Paper elevation={3} >
-        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'
+import {createTheme,ThemeProvider} from '@mui/material';
+const theme=createTheme({
+    components:{
+        MuiTypography:{
+        variants:[
+            {
+                props:{
+                    variant:"body2",
+                },
+                style:{
+                    fontSize:11,
+                }
+            },
+            {
+                props:{
+                    variant:"body3",
+                },
+                style:{
+                    fontSize:9,
+                }
+            }
+        ]
+    }
+}
+})
+const TourCard = ({tour}) => {
+    return <Grid item xs={3}>
+        <ThemeProvider theme={theme}>
+        <Paper elevation={3} >
+        <img src={tour.image}
             alt=''
             className='img' />
         <Box paddingX={1}>
-            <Typography variant='subtitle1' component="h2">Breakfast</Typography>
+            <Typography variant='subtitle1' component="h2">{tour.name}</Typography>
             <Box sx={{ display: "flex", alignItems: "center" }}>
                 <AccessTime sx={{ width: 12.5 }} />
-                <Typography ml={0.5} variant='body2' component="p">5 hours</Typography>
+                <Typography ml={0.5} variant='body2' component="p">{tour.duration}</Typography>
             </Box>
             <Box sx={{ display: "flex", alignItems: "center" }} mt={3}>
-                <Rating readOnly size="small" name='read-only' value={4.5} precision={0.5} />
-                <Typography ml={0.5} variant='body2' component="p">4.5</Typography>
-                <Typography ml={0.5} variant='body2' component="p">(655 reviews)</Typography>
+                <Rating readOnly size="small" name='read-only' value={tour.rating} precision={0.5} />
+                <Typography ml={0.5} variant='body2' component="p">{tour.rating}</Typography>
+                <Typography ml={0.5} variant='body2' component="p">({tour.numberOfReviews} reviews)</Typography>
             </Box>
             <Box>
-            <Typography mt={0} variant='h6' component="h3">From C $425</Typography>
+            <Typography mt={0} variant='h6' component="h3">From C {tour.price}</Typography>
    
             </Box>
         </Box>
     </Paper>
+    </ThemeProvider>
     </Grid>
 }
 
